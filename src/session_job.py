297ee8ccbac616872152fb731e0a67cb0c43237e
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-03-21 10:52:42 +0100

    introduced session aggregation functionality to src.session_job.py

--- src/session_job.py
+++ src/session_job.py
@@ -3,6 +3,26 @@ from pyflink.table import EnvironmentSettings, DataTypes, TableEnvironment, Stre
 from pyflink.common.watermark_strategy import WatermarkStrategy
 from pyflink.common.time import Duration
 
+def create_taxi_events_aggSessionW_sink(t_env):
+    table_name = 'taxi_events_agg_PUDO_5minSessionDOtime'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            event_hour TIMESTAMP(3),
+            PU_DO_ID_pair VARCHAR,
+            num_trips BIGINT,
+            PRIMARY KEY (event_hour, PU_DO_ID_pair) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+        """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
 def create_taxi_events_aggTumbleW_sink(t_env):
     table_name = 'taxi_events_agg_PUDO_5minTumbleDOtime'
     sink_ddl = f"""
@@ -50,6 +70,46 @@ def create_events_source_kafka(t_env):
     t_env.execute_sql(source_ddl)
     return table_name
 
+def log_aggregation_session():
+    # Set up the execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+    env.set_parallelism(3)
+
+    # Set up the table environment
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    watermark_strategy = (
+        WatermarkStrategy
+        .for_bounded_out_of_orderness(Duration.of_seconds(5))
+        .with_timestamp_assigner(
+            # This lambda is your timestamp assigner:
+            #   event -> The data record
+            #   timestamp -> The previously assigned (or default) timestamp
+            lambda event, timestamp: event[2]  # We treat the second tuple element as the event-time (ms).
+        )
+    )
+    try:
+        # Create Kafka table
+        source_table = create_events_source_kafka(t_env)
+        aggregated_table = create_taxi_events_aggSessionW_sink(t_env)
+
+        t_env.execute_sql(f"""
+        INSERT INTO {aggregated_table}
+        SELECT
+            window_start AS event_hour,
+            PU_DO_ID_pair,
+            COUNT(*) AS num_trips
+        FROM TABLE(
+          SESSION(TABLE {source_table}, DESCRIPTOR(dropoff_timestamp), INTERVAL '5' MINUTE)
+          )
+        GROUP BY window_start, PU_DO_ID_pair ;
+        """).wait()
+
+    except Exception as e:
+        print("Writing records from Kafka to JDBC failed:", str(e))
+
 def log_aggregation_tumble():
     # Set up the execution environment
     env = StreamExecutionEnvironment.get_execution_environment()
@@ -92,4 +152,5 @@ def log_aggregation_tumble():
 
 
 if __name__ == '__main__':
-    log_aggregation_tumble()
+    # log_aggregation_tumble()
+    log_aggregation_session()
