dfa58b1ddf5dc15b071ecd05b491812e74d6469d
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-03-21 08:27:07 +0100

    src.taxi_clean_job.py: removed unused log_aggregation function

--- src/taxi_clean_job.py
+++ src/taxi_clean_job.py
@@ -81,48 +81,5 @@ def log_processing():
     except Exception as e:
         print("Writing records from Kafka to JDBC failed:", str(e))
 
-def log_aggregation():
-    # Set up the execution environment
-    env = StreamExecutionEnvironment.get_execution_environment()
-    env.enable_checkpointing(10 * 1000)
-    env.set_parallelism(3)
-
-    # Set up the table environment
-    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
-    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
-
-    watermark_strategy = (
-        WatermarkStrategy
-        .for_bounded_out_of_orderness(Duration.of_seconds(5))
-        .with_timestamp_assigner(
-            # This lambda is your timestamp assigner:
-            #   event -> The data record
-            #   timestamp -> The previously assigned (or default) timestamp
-            lambda event, timestamp: event[2]  # We treat the second tuple element as the event-time (ms).
-        )
-    )
-    try:
-        # Create Kafka table
-        source_table = create_events_source_kafka(t_env)
-        aggregated_table = create_events_aggregated_sink(t_env)
-
-        t_env.execute_sql(f"""
-        INSERT INTO {aggregated_table}
-        SELECT
-            window_start as event_hour,
-            test_data,
-            COUNT(*) AS num_hits
-        FROM TABLE(
-            TUMBLE(TABLE {source_table}, DESCRIPTOR(event_watermark), INTERVAL '1' MINUTE)
-        )
-        GROUP BY window_start, test_data;
-        
-        """).wait()
-
-    except Exception as e:
-        print("Writing records from Kafka to JDBC failed:", str(e))
-
-
 if __name__ == '__main__':
     log_processing()
-    # log_aggregation()
